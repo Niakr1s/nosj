814a53ca1dd5326ca5ba4153241df6dce79d81b3
Niakr1s <pavel2188@gmail.com>
2020-05-21 16:31:42 +0300

    impl Array.Parse(), added some tests

--- array.go
+++ array.go
@@ -11,5 +11,27 @@ func NewArray() *Array {
 }
 
 func (a *Array) Parse(s *bufio.Scanner) Node {
+	if s.Text() != `[` {
+		return nil
+	}
+	s.Scan()
+	SkipSpaces(s)
+
+	for {
+		a.items = append(a.items, Parse(s))
+		SkipSpaces(s)
+		if s.Text() == "," {
+			s.Scan()
+			continue
+		}
+		break
+	}
+
+	SkipSpaces(s)
+
+	if s.Text() == "]" {
+		s.Scan()
+	}
+
 	return a
 }
--- parse_test.go
+++ parse_test.go
@@ -1,24 +1,37 @@
 package nosj
 
 import (
-	"bufio"
 	"reflect"
 	"testing"
 )
 
 func TestParse(t *testing.T) {
-	type args struct {
-		s *bufio.Scanner
-	}
+	obj := &Object{items: map[string]Node{"name": &String{"Pavel"}}}
+	obj2 := &Object{items: map[string]Node{"name": &String{"Pavel"}, "name1": &String{"Pavel1"}}}
+	arr := &Array{items: []Node{obj, obj}}
+	arr2 := &Array{items: []Node{obj2, obj2}}
+
 	tests := []struct {
 		name                string
 		inputStr            string
 		want                Node
 		expectedRemainedStr string
 	}{
-		{"simple object: value - string", `{"name" : "Pavel"}`,
-			&Object{items: map[string]Node{"name": &String{"Pavel"}}},
-			""},
+		{"object with 1 key, value - string",
+			`{"name" : "Pavel"}`,
+			obj, ""},
+
+		{"object with multiple keys, value - string",
+			`{"name" : "Pavel", "name1" : "Pavel1"}`,
+			obj2, ""},
+
+		{"array with 2 objects with 1 key: value - string",
+			`[{"name" : "Pavel"},{"name" : "Pavel"}]`,
+			arr, ""},
+
+		{"array with 2 objects with 2 key: value - string",
+			`[{"name" : "Pavel", "name1" : "Pavel1"},{"name" : "Pavel", "name1" : "Pavel1"}]`,
+			arr2, ""},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
